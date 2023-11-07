71e8a3a0fbee679d309518c5b6975bce1353a4a0
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2023-11-07 16:25:16 -0300

    implements a method to handle backpressure

--- lib/src/readable_stream_source/readable_stream_source.compatibility_layer.dart
+++ lib/src/readable_stream_source/readable_stream_source.compatibility_layer.dart
@@ -1,8 +1,19 @@
 part of 'readable_stream_source.dart';
 
+ReadableStreamSource<T> createReadableStreamSourceFromStream<T>(
+  Stream<T> stream, [
+  bool handleBackPressure = false,
+]) {
+  if (handleBackPressure) {
+    return _createReadableStreamSourceFromStreamWithBackPressure(stream);
+  } else {
+    return _createReadableStreamSourceFromStream(stream);
+  }
+}
 
 /// Create [ReadableStreamSource] from Dart [Stream].
-ReadableStreamSource<T> createReadableStreamSourceFromStream<T>(Stream<T> stream) {
+ReadableStreamSource<T> _createReadableStreamSourceFromStream<T>(
+    Stream<T> stream) {
   late final StreamSubscription<T> subscription;
   return ReadableStreamSource(
     start: allowInterop((controller) {
@@ -19,4 +30,56 @@ ReadableStreamSource<T> createReadableStreamSourceFromStream<T>(Stream<T> stream
       (reason) async => subscription.cancel(),
     ),
   );
+}
+
+// TODO: remove the print statements
+// TODO: pass a callback to pass the progress update
+
+/// Create [ReadableStreamSource] from Dart [Stream].
+/// This version of the function is used for browsers that support backpressure.
+ReadableStreamSource<T>
+    _createReadableStreamSourceFromStreamWithBackPressure<T>(Stream<T> stream) {
+  late final StreamSubscription<T> subscription;
+
+  final readableStream = ReadableStreamSource<T>(
+    start: allowInterop((controller) {
+      var uploadedData = 0;
+
+      subscription = stream.listen(
+        (event) async {
+          controller.enqueue(event);
+
+          print(controller.desiredSize);
+
+          if (controller.desiredSize < 0) {
+            subscription.pause();
+
+            while (true) {
+              await Future.delayed(const Duration(milliseconds: 300), () {});
+              if (controller.desiredSize > 0) {
+                print('desiredSize > 0');
+                print(controller.desiredSize);
+                print('subscription.resume()');
+                subscription.resume();
+                break;
+              }
+            }
+          }
+
+          uploadedData += (event as List<int>).length;
+          print('uploadedData: $uploadedData');
+        },
+        onDone: () {
+          print(
+              'onDone: uploadedData: $uploadedData, desiredSize: ${controller.desiredSize}');
+          controller.close();
+        },
+      );
+    }),
+    cancel: allowInterop(
+      (reason) async => subscription.cancel(),
+    ),
+  );
+
+  return readableStream;
 }
